eab929b6e69b563e27467f1a7dff48e03ff75d53
Laura G <laura_g56@hotmail.com>
2021-05-03 22:23:05 +1000

    Updated Weather and components

--- src/Weather.js
+++ src/Weather.js
@@ -1,41 +1,27 @@
 import React from "react";
+import FormattedDate from "./FormattedDate";
+import Icon from "./Icon";
+import Temperature from "./Temperature";
 
 import "./Weather.css";
 
-export default function Weather() {
-  let weatherData = {
-    dateTime: "Friday 23 Apr 2021 5:36 PM",
-    city: "Melbourne, AU",
-    image: "images/partly-cloudy.svg",
-    description: "Cloudy",
-    temperature: 24,
-    humidity: 25,
-    wind: 13,
-    rain: 17
-  };
+export default function Weather(props) {
   return (
     <div className="Weather">
       <p className="date" id="dayAndTime">
-        {weatherData.dateTime}
+        <FormattedDate date={props.data.date} />
       </p>
-      <h2 id="city">{weatherData.city}</h2>
-      <img id="icon" src={weatherData.image} alt="clouds" />
+      <h2 id="city">
+        {props.data.city}, {props.data.country}
+      </h2>
+      <Icon code={props.data.icon} size={85} />
       <p className="description" id="description">
-        {weatherData.description}
+        {props.data.description}
       </p>
 
       <div className="temperature">
         <p>
-          <span id="temperature">{weatherData.temperature}</span>
-          <span className="units">
-            <a href="/" id="celsius-link">
-              °C
-            </a>{" "}
-            |
-            <a href="/" id="fahrenheit-link">
-              °F
-            </a>
-          </span>
+          <Temperature celsius={props.data.temperature} />
         </p>
       </div>
 
@@ -43,15 +29,15 @@ export default function Weather() {
         <ul>
           <li>
             Wind: <span id="wind"></span>
-            {weatherData.wind}km/h
+            {props.data.wind}km/h
           </li>
           <li>
             Humidity: <span id="humidity"></span>
-            {weatherData.humidity}%
+            {props.data.humidity}%
           </li>
           <li>
-            Chance of rain: <span id="precipitation"></span>
-            {weatherData.rain}%
+            Chance of rain: <span id="precipitation">3</span>
+            {props.data.rain}%
           </li>
         </ul>
       </div>

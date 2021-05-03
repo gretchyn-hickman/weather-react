fcc184aa0cb0c9bbf5cc3959b85a3338d8d31259
Laura G <laura_g56@hotmail.com>
2021-05-03 22:22:33 +1000

    Added temp and units components

---
+++ src/Temperature.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import "./Temperature.css";
+
+export default function Temperature(props) {
+  const [unit, setUnit] = useState("celsius");
+
+  function toFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function toCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="Temperature" id="temperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="units">
+          °C |{" "}
+          <a href="/" onClick={toFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+
+    return (
+      <div className="Temperature" id="temperature">
+        <span className="temperature">{Math.round(fahrenheit)}</span>
+        <span className="units">
+          <a href="/" onClick={toCelsius}>
+            °C
+          </a>{" "}
+          | °F
+        </span>
+      </div>
+    );
+  }
+}
